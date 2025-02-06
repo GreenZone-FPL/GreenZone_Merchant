8b9ea7254bf4ddd0bb52d4fa4c8e3e5814764041
ngocdai99 <ngocdai99@gmail.com>
2025-02-06 23:45:14 +0700

    Update UI Login Screen

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -2,19 +2,22 @@ import React, { useState } from 'react';
 import {
   Image,
   KeyboardAvoidingView,
+  Pressable,
   ScrollView,
   StyleSheet,
   Text
 } from 'react-native';
-import { Column, FlatInput, LightStatusBar, PrimaryButton, Row } from '../../components';
-import { colors, GLOBAL_KEYS } from '../../constants';
 import { Switch } from 'react-native-paper';
+import { Column, FlatInput, LightStatusBar, PrimaryButton, Row, NormalText } from '../../components';
+import { colors, GLOBAL_KEYS } from '../../constants';
 
 
 const LoginScreen = props => {
   const [maNv, setMaNv] = useState();
   const [password, setPassword] = useState();
   const [checked, setChecked] = useState();
+  const [isPasswordVisible, setIsPasswordVisible] = useState();
+
   return (
     <ScrollView style={styles.container}>
       <KeyboardAvoidingView>
@@ -31,16 +34,18 @@ const LoginScreen = props => {
               value={maNv}
               label="Mã nhân viên"
               style={{ width: '100%' }}
-              placeholder="Nhập số điện thoại của bạn..."
+              placeholder="Nhập mã nhân viên của bạn"
               setValue={setMaNv}
             />
             <FlatInput
               value={password}
               label="Mật khẩu"
               style={{ width: '100%' }}
-              placeholder="Nhập số điện thoại của bạn..."
+              placeholder="Nhập mật khẩu của bạn"
               setValue={setPassword}
               keyboardType=""
+              isPasswordVisible={isPasswordVisible}
+              setIsPasswordVisible={setIsPasswordVisible}
               secureTextEntry={true}
             />
 
@@ -51,10 +56,13 @@ const LoginScreen = props => {
                   color={colors.primary}
                   onValueChange={(value) => setChecked(value)}
                 />
-                <Text>Ghi nhớ tôi</Text>
+                <NormalText text='Ghi nhớ tôi' />
+
               </Row>
 
-              <Text style={{ color: colors.primary, fontWeight: '600' }}>Quên mật khẩu?</Text>
+              <Pressable >
+                <NormalText text='Quên mật khẩu?' style={{ color: colors.primary, fontWeight: '500' }} />
+              </Pressable>
             </Row>
             <PrimaryButton
               title="Đăng nhập"
@@ -107,57 +115,6 @@ const styles = StyleSheet.create({
     fontWeight: '800',
     color: colors.primary,
   },
-  row: {
-    flexDirection: 'row',
-    alignContent: 'center',
-    alignItems: 'center',
-    gap: 10,
-    paddingHorizontal: GLOBAL_KEYS.PADDING_DEFAULT,
-  },
-  separator: {
-    flex: 1,
-    height: 1,
-    backgroundColor: colors.primary,
-  },
-  other: {
-    fontSize: GLOBAL_KEYS.TEXT_SIZE_DEFAULT,
-    color: colors.gray700,
-    fontWeight: '500',
-  },
-  fbLoginBtn: {
-    backgroundColor: colors.blue600,
-    flexDirection: 'row',
-    width: '100%',
-    padding: GLOBAL_KEYS.PADDING_DEFAULT,
-    borderRadius: GLOBAL_KEYS.BORDER_RADIUS_DEFAULT,
-    alignContent: 'center',
-    alignItems: 'center',
-    gap: GLOBAL_KEYS.GAP_SMALL,
-    justifyContent: 'center',
-  },
-  textFb: {
-    textAlign: 'center',
-    color: colors.white,
-    fontWeight: '500',
-    fontSize: GLOBAL_KEYS.TEXT_SIZE_DEFAULT,
-  },
-  googleLoginBtn: {
-    backgroundColor: colors.white,
-    flexDirection: 'row',
-    width: '100%',
-    padding: GLOBAL_KEYS.PADDING_DEFAULT,
-    borderRadius: GLOBAL_KEYS.BORDER_RADIUS_DEFAULT,
-    alignContent: 'center',
-    alignItems: 'center',
-    gap: GLOBAL_KEYS.GAP_SMALL,
-    justifyContent: 'center',
-    borderColor: colors.gray200,
-    borderWidth: 1,
-  },
-  textGoogle: {
-    textAlign: 'center',
-    color: colors.black,
-    fontWeight: '500',
-    fontSize: GLOBAL_KEYS.TEXT_SIZE_DEFAULT,
-  },
+
+
 });
