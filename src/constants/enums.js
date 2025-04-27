f5b38ce05cf5a96bd9b392101810f7b656edf9c4
ngocdai99 <ngocdai99@gmail.com>
2025-04-27 21:02:36 +0700

    fix socket

--- src/constants/enums.js
+++ src/constants/enums.js
@@ -11,7 +11,7 @@ export const PaymentMethod = Object.freeze({
 export const OrderStatus = Object.freeze({
   AWAITING_PAYMENT: {label: 'Chờ thanh toán', value: 'awaitingPayment'},
   PENDING_CONFIRMATION: {label: 'Chờ xác nhận', value: 'pendingConfirmation'},
-  PROCESSING: {label: 'Đang xử lý', value: 'processing'},
+  PROCESSING: {label: 'Đang chuẩn bị', value: 'processing'},
   READY_FOR_PICKUP: {label: 'Chờ lấy hàng', value: 'readyForPickup'},
   SHIPPING_ORDER: {label: 'Đang giao hàng', value: 'shippingOrder'},
   COMPLETED: {label: 'Hoàn thành', value: 'completed'},
--- src/sevices/merchantSocketService.js
+++ src/sevices/merchantSocketService.js
@@ -54,7 +54,8 @@ class MerchantSocketService {
            New Order: {"message": " Đơn hàng mới #67e036a784526a4a39d6509e cần xử lý trước
            3/18/2025, 9:28:15 PM", "orderId": "67e036a784526a4a39d6509e", "storeId": "67b68d7698c1fc822e49fabd"}
            */
-           socket.emit('order.join', data.orderId);
+           this.socket.emit('order.join', data.orderId);
+           console.log('emit order join')
           if (orderNewCallback) {
             orderNewCallback(data);
             console.log('Callback executed');
@@ -104,7 +105,7 @@ class MerchantSocketService {
 
 
 
-        
+
 
         this.socket.on('disconnect', () => {
           console.log('Disconnected');
